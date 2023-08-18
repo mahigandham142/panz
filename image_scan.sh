b8fc5d0647f52f8ea0dc2e493c82cf652478c974
mahigandham142 <126136131+mahigandham142@users.noreply.github.com>
2023-08-18 16:13:03 +0530

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-repo_name=demo
-image_tag=SAMPLE-PROJECT-${BUILD_NUMBER}
+repo_name=sample
+image_tag=Gandham-6
 arn=arn:aws:sns:ap-south-1:351836203514:sample
 critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
 high_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i high | cut -d ":" -f 2 | tr -d ",")
 
-if [ -z "$critical_vulnr" ]; then
-    critical_vulnr=0
+# Set default values to 0 if the variables are empty
+if [ -z "$critical_vulnr" ]
+then
+        critical_vulnr=0
 fi
 
-if [ -z "$high_vulnr" ]; then
-    high_vulnr=0
+if [ -z "$high_vulnr" ]
+then
+        high_vulnr=0
 fi
 
 echo "High vulnerabilities: $high_vulnr"
 echo "Critical vulnerabilities: $critical_vulnr"
 
-if [[ $high_vulnr -gt 0 && $critical_vulnr -ge 0  ]]
+if [[ $high_vulnr -gt 0 && $critical_vulnr -ge 0 ]]
 then
-    echo "Your image has high vulnerabilities" && aws sns publish --topic-arn $arn --message "Image vulnerabilities: High vulnerabilities detected for image $image_tag in repository $repo_name" --subject "Image Vulnerability Alert"
-
-    exit 1 
+   echo "your image is having higher vulnerabilities,please check...." && aws sns publish --topic-arn $arn --message "your image in this $repo_name repo in this $image_tag image is having higher vulnerabilities,please check..."
+    exit 1  # This will exit the script with a non-zero status code
 else
-    echo "Your image has low vulnerabilities"
+    echo "your image is safe for deployment"
 fi
