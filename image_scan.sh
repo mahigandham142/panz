770ab3b044d37a1ee6980aebc83471274f9c9ed1
mahigandham142 <126136131+mahigandham142@users.noreply.github.com>
2023-09-11 19:41:25 +0530

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repo_name=demo
+repo_name=sample
 image_tag=SAMPLE-PROJECT-${BUILD_NUMBER}
 arn=arn:aws:sns:ap-south-1:207203418893:sample
 critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
