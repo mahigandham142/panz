59ffaa53f76e57f9249c5b3ea5cbee8868f00481
mahigandham142 <126136131+mahigandham142@users.noreply.github.com>
2023-10-21 09:04:45 +0530

    Update ecs-auto.sh

--- ecs-auto.sh
+++ ecs-auto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SERVICE="demo"
 CLUSTER_NAME="demo"
-AWS_REGION="ap-south-1"
+AWS_REGION="us-east-2"
 
 # Register a new Task definition 
-aws ecs register-task-definition --family demo --cli-input-json file://task-new.json --region $AWS_REGION
+aws ecs register-task-definition --family demo-new-oct --cli-input-json file://task-new.json --region $AWS_REGION
 
 # Update Service in the Cluster
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition demo --desired-count 1 --region $AWS_REGION
+aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition demo-new-oct --desired-count 1 --region $AWS_REGION
 
