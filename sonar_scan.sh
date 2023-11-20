2e1b0273f71d9e74c6bbd7ad388d2ba4b8edd34a
mahigandham142 <126136131+mahigandham142@users.noreply.github.com>
2023-11-20 20:45:52 +0530

    Update sonar_scan.sh

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -u admin:Mahi@123  http://af92004913f324520ba8606446672f5d-467569492.us-east-2.elb.amazonaws.com:9000/api/qualitygates/project_status?projectKey=sonar > demo.json
 sonar_scan=$(cat demo.json | grep -i status | cut -d ':' -f 3 | cut -d ',' -f 1 | tr -d '"')
-if [[ $? -eq 0 ]]
+if [[ sonar_scan -eq ok ]]
 then
     echo "sonar_scan_status is $sonar_scan"
 else
