d79c61d8893058cb936d71d6dabaf58802dc8519
mahigandham142 <126136131+mahigandham142@users.noreply.github.com>
2023-12-03 10:38:50 +0530

    Update sonar_scan.sh

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -u admin:mahesh@123  http://a73651e2fcb4a4f08acea3340884f16a-951601558.us-east-2.elb.amazonaws.com:9000//api/qualitygates/project_status?projectKey=dotnet-app > demo.json
+curl -u admin:mahesh@123  http://a73651e2fcb4a4f08acea3340884f16a-951601558.us-east-2.elb.amazonaws.com:9000/api/qualitygates/project_status?projectKey=dotnet-app > demo.json
 sonar_scan=$(cat demo.json | grep -i status | cut -d ':' -f 3 | cut -d ',' -f 1 | tr -d '"')
 if [[ sonar_scan -eq ok ]]
 then
